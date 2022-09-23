68858bda731afc52f1e920eee06b7e28f40f772c
Tippawan Kopanklang <b6334000@g.sut.ac.th>
2022-09-23 16:52:52 +0700

    first commit

---
+++ components/Dialog.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-dialog
+        v-model="getDialog.isShow"
+        max-width="290"
+        persistent
+    >
+        <v-card>
+            <v-card-title>{{getDialog.title}}</v-card-title>
+            <v-card-text v-html="getDialog.message"></v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                    color = "purple lighten-1"
+                    text
+                    @click="hideDialog"
+                >
+                    Close
+                </v-btn>
+
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    computed: {
+        getDialog(){
+            return this.$store.getters.getDialog
+        }
+    },
+    methods: {
+        hideDialog(){
+            this.$store.dispatch('setDialog',{
+              isShow: false,
+              title: '',
+              message: ''  
+            })
+        }
+    }
+}
+</script>
---
+++ static/layouts/default.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app>
+    <nuxt></nuxt>
+    <Dialog/>
+  </v-app>
+</template>
+
+<script>
+import Dialog from '~/components/Dialog'
+
+export default{
+  components: {
+    Dialog
+  }
+}
+</script>
+
+<style lang="scss">
+    .v-application{
+        font-family: 'Quicksand', sans-serif;
+    }
+    .v-toolbar__title{
+        text-align: center;
+        width: 100%;
+        font-weight: bold
+    }
+    .v-application.purple{
+      background-color:#6a1b9a !important;
+      border-color:#6a1b9a !important;
+    }
+    .text-purple{
+      color: #6a1b9a;
+    }
+    .text-main{
+      font-size: 20px;
+    }
+    .text-title{
+      font-size: 20px;
+      font-weight: bold;
+    }
+    .w-100{
+      width: 100%;
+    }
+    .w-80{
+      width: 80%
+    }
+    .my-btn{
+      font-size: 20px !important;
+      text-transform: none !important;
+      height: auto !important;
+      padding: 10px 0;
+      font-weight: bold;
+      
+    }
+    .outlined{
+        min-height: 35px;
+        border: 1.5px solid #6a1b9a;
+        border-radius: 28px;
+        line-height: normal;
+      }
+    .set-padding{
+      padding: 0 10px;
+    }
+</style>
